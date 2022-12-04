7a088265afca05704152308c136df17c7848cc3c
Kota <maedakoutaa@gmail.com>
2022-12-04 11:28:53 +0900

    initial commit

---
+++ src/main/java/com/example/MiniServerApp04WebSocket04/MiniServerApp04WebSocket04Application.java
@@ -0,0 +1,13 @@
+package com.example.MiniServerApp04WebSocket04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniServerApp04WebSocket04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniServerApp04WebSocket04Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MiniServerApp04WebSocket04/MiniServerApp04WebSocket04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiniServerApp04WebSocket04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniServerApp04WebSocket04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
